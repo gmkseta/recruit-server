42126ec84a99ec62a35b4f8dba193fa19d22a73e
seenewmax <donaskwhy@nate.com>
2019-08-06 03:29:03 +0900

    signup, signin form added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :form_sheets
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root 'home#index'
+
+  resources :form_sheets
+  resources :home do
+    collection do
+      get :signin
+      get :signup
+    end
+  end
 end
