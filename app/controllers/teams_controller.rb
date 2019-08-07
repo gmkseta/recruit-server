0d7f09e82bd2b647e80d6270ef91f948bff5bc96
chulhee23 <chulhee23@naver.com>
2019-08-07 23:26:36 +0900

    team 생성하기

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,45 @@
+class TeamsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :team_params, only: %i[create update]
+  before_action :load_object, only: %i[show edit update destroy]
+  def index
+    @teams = Team.all
+  end
+  
+  def new
+    @team = Team.new
+  end
+  
+  def create
+    @team = Team.create!(team_params)
+    TeamUser.create!(team: @team, user: current_user, role: :captain)
+    redirect_to team_path(@team)
+  end
+  
+  def show
+    @main_captain = @team.team_users.captain.first.user
+    
+  end
+
+  def edit
+  
+  end
+
+  def update
+  
+  end
+
+  def destroy
+  
+  end
+
+  private
+  def load_object
+    @team = Team.find(params[:id])
+  end
+  
+  def team_params
+    params.fetch(:team,{}).permit(:name, :description)
+  end
+
+end
