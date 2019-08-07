1680b943da881b3dec6f730b98c1fb208b4400e2
gmkseta <gmkseta@gmail.com>
2019-08-08 04:10:23 +0900

    ui 그켬

--- app/services/generate_form.rb
+++ app/services/generate_form.rb
@@ -2,6 +2,7 @@ class GenerateForm
   attr_reader :question
  
   def initialize(params, answer=true)
+    
     form_sheet = FormSheet.find(params[:form_sheet_id])
     
     if answer
@@ -12,6 +13,8 @@ class GenerateForm
     else
       @question= form_sheet.questions.create({questionable: Formatter.for( params[:type]), status: "not_answer"})
     end
+  rescue Exception => e
+    p e
   end
 
   private
