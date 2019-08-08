0c7c9108ef5bba647cbc54b896959b53387b2376
chulhee23 <chulhee23@naver.com>
2019-08-09 00:38:14 +0900

    seed 데이터 생성하기

---
+++ app/services/generate_seed.rb
@@ -0,0 +1,26 @@
+class GenerateSeed
+  def self.exec
+    generate_users_teams
+    generate_rand_apply
+  end
+
+  private
+  def self.generate_users_teams
+     12.times do |i|
+       u = User.create!(email: "test#{i}@test.com", password: "asdasd", password_confirmation: "asdasd", name: "유저-#{i}")
+       t = Team.create!(name: "#{i}번째 팀", description: "#{i}번째 설명입니다.")
+       TeamUser.create!(team: t, user: u, role: :captain)
+     end
+  end
+
+  def self.generate_rand_apply
+    12.times do |i|
+      u = User.all.sample
+      t = Team.all.sample
+      unless u.my_team?(t)
+        TeamUser.create!(user: u, team: t, role: :apply)
+      end
+    end
+  end
+
+end
