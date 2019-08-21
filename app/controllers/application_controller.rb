040afd6b5d5e5bdffd3ed74484e2b36aaed113a1
chulhee23 <chulhee23@naver.com>
2019-08-21 11:42:48 +0900

    pull

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  
+
   #추가
   def configure_permitted_parameters
     added_attrs = %i{name image phone}
